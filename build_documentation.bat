ae7dc3fece8cacfd0c2b05c1397b108eca5638d7
James M. Bishop <james.mckown.bishop@gmail.com>
2020-08-25 17:19:31 -0400

    Added conventional Anaconda path

--- build_documentation.bat
+++ build_documentation.bat
@@ -7,6 +7,7 @@ rem | This file requires an Anaconda (or Miniconda) installation
 rem | You may need to specify your local path to activate.bat
 
 call C:/Users/%username%/Miniconda3/Scripts/activate.bat
+call C:/Users/%username%/AppData/Local/Continuum/anaconda3/Scripts/activate.bat
 pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --upgrade sphinx==2.4.4 sphinx_rtd_theme m2r
 cd docs/
 sphinx-build . _build/
