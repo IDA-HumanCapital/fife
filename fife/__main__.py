f0fe95d1fe44922c6ec146cd91c67191e59e4c9c
Jain <ajain@ida.org>
2020-10-05 17:16:11 -0400

    Update __main__.py
    Added 'by_feature' which allows for the creation of separate fife models for each value of that feature. If configuration parameter is provided, output all new files under subfolders, otherwise remain the same.

--- fife/__main__.py
+++ fife/__main__.py
@@ -34,171 +34,188 @@ def main():
         raise NotImplementedError(
             "Forecasting future state from the command line is not yet supported. Try LGBStateModeler from the FIFE Python package."
         )
+
     utils.make_results_reproducible(config["SEED"])
     utils.redirect_output_to_log(path=config["RESULTS_PATH"])
     utils.print_copyright()
     utils.print_config(config)
-    data = read_data(config)
+    df = read_data(config)
     print(f"I/O setup time: {time() - checkpoint_time} seconds")
 
-    checkpoint_time = time()
-    data_processor = processors.PanelDataProcessor(config, data)
-    data_processor.build_processed_data()
-    utils.save_intermediate_data(
-        data_processor.data,
-        "Processed_Data",
-        file_format="pickle",
-        path=config["RESULTS_PATH"],
-    )
-    print(f"Data processing time: {time() - checkpoint_time} seconds")
-
-    checkpoint_time = time()
-    utils.ensure_folder_existence(f'{config["RESULTS_PATH"]}/Intermediate/Models')
-    test_intervals = config.get("TEST_INTERVALS", config.get("TEST_PERIODS", 0) - 1)
-    if config.get("TREE_MODELS"):
-        modeler_class = lgb_modelers.LGBSurvivalModeler
-    elif config.get("PROPORTIONAL_HAZARDS"):
-        modeler_class = tf_modelers.ProportionalHazardsModeler
-    else:
-        modeler_class = tf_modelers.FeedforwardNeuralNetworkModeler
-    modeler = modeler_class(config=config, data=data_processor.data)
-    modeler.n_intervals = (
-        test_intervals if test_intervals > 0 else modeler.set_n_intervals()
-    )
-    if config.get("HYPER_TRIALS", 0) > 0:
-        params = modeler.hyperoptimize(config["HYPER_TRIALS"])
-    else:
-        params = None
-    modeler.build_model(n_intervals=modeler.n_intervals, params=params)
-    modeler.save_model(path=f"{config['RESULTS_PATH']}/Intermediate/Models/")
-    print(f"Model training time: {time() - checkpoint_time} seconds")
-
-    checkpoint_time = time()
-    if test_intervals > 0:
-
-        # Save metrics
-        evaluation_subset = modeler.data["_period"] == (
-            modeler.data["_period"].max() - test_intervals
+    by_feature = config.get("BY_FEATURE", None)
+    if by_feature != None and by_feature not in df.columns():
+        raise ValueError(
+            "The selected feature for 'BY_FEATURE' is not in the dataset. Check spelling or the original dataset to ensure that you are entering the correct feature name."
         )
-        utils.save_output_table(
-            modeler.evaluate(evaluation_subset),
-            "Metrics",
-            path=config["RESULTS_PATH"],
-        )
+    values = [""]
+    if by_feature != None:
+        values = list(set(df[by_feature]))
 
-        # Save counts by quantile
-        utils.save_output_table(
-            modeler.tabulate_survival_by_quantile(
-                n_quantiles=config["QUANTILES"],
-                subset=evaluation_subset,
-            ),
-            "Counts_by_Quantile",
-            index=False,
-            path=config["RESULTS_PATH"],
+    for feature_val in values:
+        checkpoint_time = time()
+        results_path = config["RESULTS_PATH"]
+        if len(values) > 1:
+            data = df[df[by_feature] == feature_val]
+            results_path = results_path + "//" + feature_val
+        else:
+            data = df
+        data_processor = processors.PanelDataProcessor(config, data)
+        data_processor.build_processed_data()
+        utils.save_intermediate_data(
+            data_processor.data,
+            "Processed_Data",
+            file_format="pickle",
+            path=results_path,
         )
+        print(f"Data processing time: {time() - checkpoint_time} seconds")
 
-        # Save forecast errors
-        actuals = np.array(
-            [
-                modeler.data[evaluation_subset]["_duration"] > time_horizon
-                for time_horizon in range(test_intervals)
-            ]
-        ).T
-        predictions = modeler.predict(evaluation_subset)
-        utils.save_output_table(
-            pd.DataFrame(
-                predictions - actuals,
-                columns=[
-                    f"{time_horizon + 1}-period Forecast Error"
+        checkpoint_time = time()
+        utils.ensure_folder_existence(f"{results_path}/Intermediate/Models")
+        test_intervals = config.get("TEST_INTERVALS", config.get("TEST_PERIODS", 0) - 1)
+        if config.get("TREE_MODELS"):
+            modeler_class = lgb_modelers.LGBSurvivalModeler
+        elif config.get("PROPORTIONAL_HAZARDS"):
+            modeler_class = tf_modelers.ProportionalHazardsModeler
+        else:
+            modeler_class = tf_modelers.FeedforwardNeuralNetworkModeler
+        modeler = modeler_class(config=config, data=data_processor.data)
+        modeler.n_intervals = (
+            test_intervals if test_intervals > 0 else modeler.set_n_intervals()
+        )
+        if config.get("HYPER_TRIALS", 0) > 0:
+            params = modeler.hyperoptimize(config["HYPER_TRIALS"])
+        else:
+            params = None
+        modeler.build_model(n_intervals=modeler.n_intervals, params=params)
+        modeler.save_model(path=f"{results_path}/Intermediate/Models/")
+        print(f"Model training time: {time() - checkpoint_time} seconds")
+
+        checkpoint_time = time()
+        if test_intervals > 0:
+
+            # Save metrics
+            evaluation_subset = modeler.data["_period"] == (
+                modeler.data["_period"].max() - test_intervals
+            )
+            utils.save_output_table(
+                modeler.evaluate(evaluation_subset),
+                "Metrics",
+                path=results_path,
+            )
+
+            # Save counts by quantile
+            utils.save_output_table(
+                modeler.tabulate_survival_by_quantile(
+                    n_quantiles=config["QUANTILES"],
+                    subset=evaluation_subset,
+                ),
+                "Counts_by_Quantile",
+                index=False,
+                path=results_path,
+            )
+
+            # Save forecast errors
+            actuals = np.array(
+                [
+                    modeler.data[evaluation_subset]["_duration"] > time_horizon
                     for time_horizon in range(test_intervals)
-                ],
-            ),
-            "Forecast_Errors",
-            index=False,
-            path=config["RESULTS_PATH"],
-        )
-
-        # Save calibration errors
-        predicted_share, actual_share = calibration_curve(
-            actuals.flatten(), predictions.flatten(), n_bins=8, strategy="quantile"
-        )
-        calibration_errors = pd.DataFrame(
-            [predicted_share, actual_share, actual_share - predicted_share]
-        ).T
-        calibration_errors.columns = [
-            "Predicted Share",
-            "Actual Share",
-            "Calibration Error",
-        ]
-        calibration_errors.index.name = "Quantile"
-        calibration_errors.index = calibration_errors.index + 1
-        utils.save_output_table(
-            calibration_errors,
-            "Calibration_Errors",
-            path=config["RESULTS_PATH"],
-        )
-
-    else:
-
-        # Save forecasts
-        individual_predictions = modeler.forecast()
-        utils.save_output_table(
-            individual_predictions, "Survival_Curves", path=config["RESULTS_PATH"]
-        )
-
-        # Save aggregated forecasts with uncertainty intervals
-        utils.save_output_table(
-            utils.compute_aggregation_uncertainty(individual_predictions),
-            "Aggregate_Survival_Bounds",
-            index=False,
-            path=config["RESULTS_PATH"],
-        )
-
-        # Save and plot actual, fitted, and forecasted retention rates
-        lead_periods = config["RETENTION_INTERVAL"]
-        time_ids = pd.factorize(
-            modeler.data[modeler.config["TIME_IDENTIFIER"]], sort=True
-        )[0]
-        retention_rates = modeler.tabulate_retention_rates(
-            lead_periods=lead_periods, time_ids=time_ids
-        )
-        utils.save_output_table(
-            retention_rates, "Retention_Rates", path=config["RESULTS_PATH"]
-        )
-        axes = retention_rates.plot()
-        axes.set_ylabel(f"{lead_periods}-period Retention Rate")
-        earliest_period = data_processor.data[
-            data_processor.config["TIME_IDENTIFIER"]
-        ].min()
-        axes.set_xlabel(f"Periods Since {earliest_period}")
-        utils.save_plot("Retention_Rates", path=config["RESULTS_PATH"])
-
-        # Plot SHAP values for a subset of observations in the final period
-        sample_size = config.get("SHAP_SAMPLE_SIZE", 0)
-        if (
-            isinstance(modeler, (lgb_modelers.GradientBoostedTreesModeler))
-            and sample_size > 0
-        ):
-            shap_observations = (
-                modeler.data[modeler.data["_predict_obs"]]
-                .sample(n=sample_size)
-                .sort_index()
-            )
-            subset = modeler.data.index.isin(shap_observations.index)
-            shap_values = modeler.compute_shap_values(subset=subset)
-            utils.plot_shap_values(
-                shap_values,
-                shap_observations[
-                    modeler.categorical_features + modeler.numeric_features
-                ],
-                modeler.data[subset][
-                    modeler.categorical_features + modeler.numeric_features
-                ],
-                config["TIME_IDENTIFIER"],
-                path=config["RESULTS_PATH"],
+                ]
+            ).T
+            predictions = modeler.predict(evaluation_subset)
+            utils.save_output_table(
+                pd.DataFrame(
+                    predictions - actuals,
+                    columns=[
+                        f"{time_horizon + 1}-period Forecast Error"
+                        for time_horizon in range(test_intervals)
+                    ],
+                ),
+                "Forecast_Errors",
+                index=False,
+                path=results_path,
             )
 
-    print(f"Output production time: {time() - checkpoint_time} seconds")
+            # Save calibration errors
+            predicted_share, actual_share = calibration_curve(
+                actuals.flatten(), predictions.flatten(), n_bins=8, strategy="quantile"
+            )
+            calibration_errors = pd.DataFrame(
+                [predicted_share, actual_share, actual_share - predicted_share]
+            ).T
+            calibration_errors.columns = [
+                "Predicted Share",
+                "Actual Share",
+                "Calibration Error",
+            ]
+            calibration_errors.index.name = "Quantile"
+            calibration_errors.index = calibration_errors.index + 1
+            utils.save_output_table(
+                calibration_errors,
+                "Calibration_Errors",
+                path=results_path,
+            )
+
+        else:
+
+            # Save forecasts
+            individual_predictions = modeler.forecast()
+            utils.save_output_table(
+                individual_predictions, "Survival_Curves", path=results_path
+            )
+
+            # Save aggregated forecasts with uncertainty intervals
+            utils.save_output_table(
+                utils.compute_aggregation_uncertainty(individual_predictions),
+                "Aggregate_Survival_Bounds",
+                index=False,
+                path=results_path,
+            )
+
+            # Save and plot actual, fitted, and forecasted retention rates
+            lead_periods = config["RETENTION_INTERVAL"]
+            time_ids = pd.factorize(
+                modeler.data[modeler.config["TIME_IDENTIFIER"]], sort=True
+            )[0]
+            retention_rates = modeler.tabulate_retention_rates(
+                lead_periods=lead_periods, time_ids=time_ids
+            )
+            utils.save_output_table(
+                retention_rates, "Retention_Rates", path=results_path
+            )
+            axes = retention_rates.plot()
+            axes.set_ylabel(f"{lead_periods}-period Retention Rate")
+            earliest_period = data_processor.data[
+                data_processor.config["TIME_IDENTIFIER"]
+            ].min()
+            axes.set_xlabel(f"Periods Since {earliest_period}")
+            utils.save_plot("Retention_Rates", path=results_path)
+
+            # Plot SHAP values for a subset of observations in the final period
+            sample_size = config.get("SHAP_SAMPLE_SIZE", 0)
+            if (
+                isinstance(modeler, (lgb_modelers.GradientBoostedTreesModeler))
+                and sample_size > 0
+            ):
+                shap_observations = (
+                    modeler.data[modeler.data["_predict_obs"]]
+                    .sample(n=sample_size)
+                    .sort_index()
+                )
+                subset = modeler.data.index.isin(shap_observations.index)
+                shap_values = modeler.compute_shap_values(subset=subset)
+                utils.plot_shap_values(
+                    shap_values,
+                    shap_observations[
+                        modeler.categorical_features + modeler.numeric_features
+                    ],
+                    modeler.data[subset][
+                        modeler.categorical_features + modeler.numeric_features
+                    ],
+                    config["TIME_IDENTIFIER"],
+                    path=results_path,
+                )
+
+        print(f"Output production time: {time() - checkpoint_time} seconds")
 
 
 def parse_config() -> dict:
