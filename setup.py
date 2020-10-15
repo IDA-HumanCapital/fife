5c91b18f247234d1c196b85408af459458b99f34
ajain-ida <69818647+ajain-ida@users.noreply.github.com>
2020-10-15 17:05:57 -0400

    Version numbers in py files pulled from VERSION.txt
    Created VERSION.txt with version number that py files pull from

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="fife",
-    version="1.3.4",
+    version=open("VERSION.txt", "r").read(),
     description=(
         "Finite-Interval Forecasting Engine: Machine learning models "
         "for discrete-time survival analysis and multivariate time series "
