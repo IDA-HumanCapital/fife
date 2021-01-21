646596b2a073e4a313dec0eae2a4ff7d81057017
Jay Dennis <jdennis@ida.org>
2021-01-21 10:29:22 -0500

    simple method for ex-post comparison of 1 period ahead exit_type estimated probabilities to actual exit type

--- tests_performance/temp.py
+++ tests_performance/temp.py
@@ -2,22 +2,33 @@ from fife.processors import PanelDataProcessor
 from fife.lgb_modelers import LGBExitModeler
 from tests_performance.Data_Fabrication import fabricate_data
 
-N_PERIODS = 5
+N_PERIODS = 6
 
-d0 = fabricate_data(N_PERSONS=100, N_PERIODS=N_PERIODS, SEED=1234, exit_prob=0.2)
+d0 = fabricate_data(N_PERSONS=10000, N_PERIODS=N_PERIODS, SEED=1234, exit_prob=0.2)
+d1 = d0.copy()
+for i in d1["ID"]:
+    if N_PERIODS in d1[d1["ID"] == i]["period"].values:
+        if (d1[(d1["ID"] == i) & (d1["period"] == N_PERIODS)]["exit_type"] != "No_exit").bool():
+            d1.loc[d1["ID"] == i, "exit_type"] = "No_exit"
 
-dp0 = PanelDataProcessor(
-    config={"CATEGORICAL_SUFFIXES": ["X1"], "NUMERIC_SUFFIXES": ["X2", "X3"]}, data=d0
+d1.drop(d0[d0["period"] == N_PERIODS].index, inplace=True)
+d1 = d1.reset_index(drop=True)
+
+dp1 = PanelDataProcessor(
+    config={"CATEGORICAL_SUFFIXES": ["X1"], "NUMERIC_SUFFIXES": ["X2", "X3"]}, data=d1
 )
-dp0.build_processed_data()
+dp1.build_processed_data()
 
-m0 = LGBExitModeler(data=dp0.data, exit_col="exit_type")
+m1 = LGBExitModeler(data=dp1.data, exit_col="exit_type")
 
-m0.build_model(parallelize=False)
+m1.build_model(parallelize=False)
 
-f0 = m0.forecast()
-f0 = f0.reset_index()
+f1 = m1.forecast()
 
-f0["period"] = N_PERIODS+1
-df = d0.merge(f0, how="right", on=["ID"])
-df
+f1 = f1.reset_index()
+f1["period"] = N_PERIODS
+df = d0.merge(f1, how="right", on=["ID", "period"])
+df2 = df.copy()
+
+fet = df["Future exit_type"].unique()
+df2.drop(df2[[i not in fet for i in df["exit_type"].values]]["exit_type"].index, inplace=True)
