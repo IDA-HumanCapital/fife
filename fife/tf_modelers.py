ddf47f6bc78a388cd3093c6c0c7e4c53bdc18351
Evan Miyakawa <emiyakaw@ida.org>
2021-06-30 09:45:09 -0400

    MC Dropout for PIs finalized, with updated Data Fab

--- fife/tf_modelers.py
+++ fife/tf_modelers.py
@@ -7,11 +7,11 @@ from warnings import warn
 from fife.base_modelers import default_subset_to_all, Modeler, SurvivalModeler
 from fife.nnet_survival import make_surv_array, surv_likelihood, PropHazards
 import numpy as np
+from random import randrange
 import optuna
 import pandas as pd
 from matplotlib import pyplot as plt
 import shap
-from random import randrange
 import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow.keras.backend as K
@@ -124,6 +124,8 @@ class TFModeler(Modeler):
         n_trials: int = 64,
         subset: Union[None, pd.core.series.Series] = None,
         max_epochs: int = 128,
+        hidden_activation: str = "sigmoid",
+        output_activation: str = "sigmoid"
     ) -> dict:
         """Search for hyperparameters with greater out-of-sample performance.
 
@@ -138,6 +140,9 @@ class TFModeler(Modeler):
             A dictionary containing the best-performing parameters.
         """
 
+        self.config["HIDDEN_ACTIVATION"] = hidden_activation
+        self.config["OUTPUT_ACTIVATION"] = output_activation
+
         def evaluate_params(
             trial: optuna.trial.Trial,
             x_train: List[np.array],
@@ -147,9 +152,13 @@ class TFModeler(Modeler):
             train_weights: np.array,
             valid_weights: np.array,
             max_epochs: int,
+            hidden_activation: str,
+            output_activation: str
         ) -> Union[None, dict]:
             """Compute out-of-sample performance for a parameter set."""
             params = {}
+            params["HIDDEN_ACTIVATION"] = hidden_activation
+            params["OUTPUT_ACTIVATION"] = output_activation
             params["BATCH_SIZE"] = trial.suggest_int(
                 "BATCH_SIZE", min(32, x_train[0].shape[0]), x_train[0].shape[0]
             )
@@ -172,7 +181,7 @@ class TFModeler(Modeler):
             )
             construction_args = getfullargspec(self.construct_embedding_network).args
             self.model = self.construct_embedding_network(
-                **{k.lower(): v for k, v in params.items() if k in construction_args}
+                **{k.lower(): v for k, v in params.items() if k.lower() in construction_args}
             )
             self.data[self.numeric_features] = self.data[self.numeric_features].fillna(
                 self.config.get("NON_CAT_MISSING_VALUE", -1)
@@ -181,7 +190,7 @@ class TFModeler(Modeler):
                 **{
                     k.lower(): v
                     for k, v in self.config.items()
-                    if k in construction_args
+                    if k.lower() in construction_args
                 }
             )
             model.compile(
@@ -256,6 +265,8 @@ class TFModeler(Modeler):
                 train_weights,
                 valid_weights,
                 max_epochs,
+                hidden_activation,
+                output_activation
             ),
             n_trials=n_trials,
         )
@@ -269,7 +280,7 @@ class TFModeler(Modeler):
             **{
                 k.lower(): v
                 for k, v in default_params.items()
-                if k in construction_args
+                if k.lower() in construction_args
             }
         )
         default_model.compile(
@@ -304,7 +315,10 @@ class TFModeler(Modeler):
         self, n_intervals: Union[None, int] = None, params: dict = None
     ) -> None:
         """Train and store a neural network, freezing embeddings midway."""
+
         tf.random.set_seed(self.config.get("SEED", 9999))
+
+
         if params is None:
             params = self.config
         if n_intervals:
@@ -316,7 +330,7 @@ class TFModeler(Modeler):
         )
         construction_args = getfullargspec(self.construct_embedding_network).args
         self.model = self.construct_embedding_network(
-            **{k.lower(): v for k, v in params.items() if k in construction_args}
+            **{k.lower(): v for k, v in params.items() if k.lower() in construction_args}
         )
         pre_freeze_early_stopping = "PRE_FREEZE_EPOCHS" not in params.keys()
         post_freeze_early_stopping = "POST_FREEZE_EPOCHS" not in params.keys()
@@ -335,6 +349,7 @@ class TFModeler(Modeler):
             )
         self.model = make_predictions_cumulative(self.model)
 
+
     def construct_embedding_network(
         self,
         dense_layers: int = 2,
@@ -342,6 +357,8 @@ class TFModeler(Modeler):
         dropout_share: float = 0.25,
         embed_exponent: float = 0,
         embed_L2_reg: float = 2.0,
+        hidden_activation: str = "sigmoid",
+        output_activation: str = "sigmoid"
     ) -> keras.Model:
         """Set embedding layers followed by alternating dropout/dense layers.
 
@@ -361,6 +378,8 @@ class TFModeler(Modeler):
         Returns:
             An untrained Keras model.
         """
+
+
         if self.categorical_features:
             categorical_input_layers = [
                 Input(shape=(1,), dtype="int32") for _ in self.categorical_features
@@ -384,13 +403,14 @@ class TFModeler(Modeler):
             numeric_input_layer = Input(shape=(len(self.numeric_features),))
             dropout_layer = Dropout(dropout_share)(numeric_input_layer)
         for _ in range(dense_layers):
-            dense_layer = Dense(nodes_per_dense_layer, activation="sigmoid")(
+            dense_layer = Dense(nodes_per_dense_layer, activation=hidden_activation)(
                 dropout_layer
             )
             dropout_layer = Dropout(dropout_share)(dense_layer)
-        output_layer = Dense(self.n_intervals, activation="sigmoid", name="output")(
+        output_layer = Dense(self.n_intervals, activation=output_activation, name="output")(
             dropout_layer
         )
+
         model = Model(
             inputs=categorical_input_layers + [numeric_input_layer],
             outputs=output_layer,
@@ -597,62 +617,45 @@ class TFModeler(Modeler):
         """
 
         subset = default_subset_to_all(subset, self.data)
-
         alpha = (1 - percent_confidence) / 2
-
         conf = str(int(percent_confidence * 100))
+        if params is None:
+            params = self.config
+        if dropout_rate is not None:
+            params["DROPOUT_SHARE"] = dropout_rate
+        if "DROPOUT_SHARE" not in params.keys():
+            warn("Model must have a dropout rate specified. Specify one using dropout_rate argument",
+                 UserWarning)
+            return None
+        dropout_model = self
 
-        def one_dropout_prediction(self, params, DROPOUT_SHARE, subset) -> pd.core.frame.DataFrame:
+        def one_dropout_prediction(self, dropout_model, params, subset) -> pd.core.frame.DataFrame:
             """Compute forecasts for one MC Dropout iteration."""
 
-            dropout_model = TFSurvivalModeler(data=self.data)
-            dropout_model.n_intervals = self.n_intervals
-
             dropout_model.config["SEED"] = randrange(1, 9999, 1)
-
-            if params is None:
-                params = dropout_model.config
-
-            if DROPOUT_SHARE is not None:
-                params["DROPOUT_SHARE"] = DROPOUT_SHARE
-
-            if "DROPOUT_SHARE" not in params.keys():
-                warn("Model must have a dropout rate specified. Specify one using dropout_rate argument",
-                     UserWarning)
-                return None
-
             dropout_model.build_model(params=params)
-
             forecasts = dropout_model.forecast()
-
             forecasts.columns = list(map(str, np.arange(1, len(forecasts.columns) + 1, 1)))
-
             ids_in_subset = dropout_model.data[subset]["ID"].unique()
-
             keep_rows = np.repeat(True, len(forecasts))
-
             for rw in range(len(forecasts)):
                 if forecasts.index[rw] not in ids_in_subset:
                     keep_rows[rw] = False
-
             forecasts = forecasts[keep_rows]
-
             return (forecasts)
+
 
         dropout_forecasts = list()
 
         # do MC dropout for n_iterations
-
         for i in range(n_iterations):
-            one_forecast = one_dropout_prediction(self, params=params,
-                                                  DROPOUT_SHARE=dropout_rate,
+            one_forecast = one_dropout_prediction(self, dropout_model = dropout_model,
+                                                  params=params,
                                                   subset = subset)
             dropout_forecasts.append(one_forecast)
 
         ### get mean forecasts
-
         sum_forecasts = dropout_forecasts[0]
-
         for i in range(1, len(dropout_forecasts)):
             sum_forecasts = sum_forecasts + dropout_forecasts[i]
 
@@ -669,16 +672,12 @@ class TFModeler(Modeler):
                     return squared_error_df
 
                 squared_error_sum_df = get_variance_for_index_time(0, time=time)
-
                 for i in range(1, len(dropout_forecasts)):
                     squared_error_sum_df = squared_error_sum_df + get_variance_for_index_time(i, time=time)
-
                 variance_df = squared_error_sum_df / len(dropout_forecasts)
-
                 return variance_df
 
             variance_all_times_df = get_forecast_variance_for_time(time=0)
-
             for t in range(1, len(mean_forecasts.columns)):
                 variance_all_times_df = pd.concat([variance_all_times_df, get_forecast_variance_for_time(time=t)],
                                                   axis=1)
@@ -692,12 +691,10 @@ class TFModeler(Modeler):
             period = np.empty(shape=0, dtype="int")
             iteration = np.empty(shape=0, dtype="int")
 
-            # iter = 0
             for iter in range(len(dropout_forecasts)):
                 observation_forecasts = dropout_forecasts[iter].iloc[rw]
                 id = dropout_forecasts[iter].index[rw]
                 survival_probability = survival_probability.append(observation_forecasts)
-
                 keys = observation_forecasts.keys()
                 period = np.append(period, observation_forecasts.keys().to_numpy(dtype="int"))
                 iteration = np.append(iteration, np.repeat(iter, len(keys)))
@@ -720,21 +717,16 @@ class TFModeler(Modeler):
                 "Upper" + conf + "PercentBound": np.empty(shape=0, dtype="float")
             })
 
-            # p = 1
             for rw in range(len(mean_forecasts.index)):
-
                 forecast_df = aggregate_observation_dropout_forecasts(dropout_forecasts, rw)
-
                 for p in range(len(forecast_df['period'].unique())):
                     period = p + 1
-                    # mean_forecasts.iloc[rw, forecast_df['period'].unique() == p
                     mean_forecast = mean_forecasts.iloc[rw, p]
                     period_forecasts = forecast_df[forecast_df['period'] == period]['survival_prob']
                     forecast_quantiles = period_forecasts.quantile([alpha, 0.5, 1 - alpha])
                     df = pd.DataFrame(
                         {"ID": forecast_df["ID"][0],
                          "Period": [period],
-                         # "Period": [forecast_df['period'].unique()[p]],
                          "Lower" + conf + "PercentBound": forecast_quantiles.iloc[0],
                          "Median": forecast_quantiles.iloc[1],
                          "Mean": mean_forecast,
@@ -742,7 +734,6 @@ class TFModeler(Modeler):
                          }
                     )
                     prediction_intervals_df = prediction_intervals_df.append(df)
-
             return prediction_intervals_df
 
         prediction_intervals_df = get_forecast_prediction_intervals(alpha)
@@ -760,18 +751,13 @@ class TFModeler(Modeler):
                 "Upper" + conf + "PercentBound": np.empty(shape=0, dtype="float")
             })
 
-            p = 0
             for p in range(len(dropout_forecasts[0].columns.unique())):
                 period = p + 1
                 dropout_expected_counts = []
-
                 for d in dropout_forecasts:
                     dropout_expected_counts.append(d[d.columns[p]].sum())
-
                 forecast_sum_series = pd.Series(dropout_expected_counts)
-
                 forecast_sum_quantiles = forecast_sum_series.quantile([alpha, 0.5, 1 - alpha])
-
                 df = pd.DataFrame(
                     {"ID": "Sum",
                      "Period": [period],
@@ -781,35 +767,25 @@ class TFModeler(Modeler):
                      "Upper" + conf + "PercentBound": forecast_sum_quantiles.iloc[2]
                      }
                 )
-
                 aggregation_pi_df = aggregation_pi_df.append(df)
-
             return aggregation_pi_df
 
         aggregation_prediction_intervals_df = get_aggregation_prediction_intervals()
-
-
-
         prediction_intervals_df = aggregation_prediction_intervals_df.append(prediction_intervals_df)
-
         prediction_intervals_df.index = np.arange(0, len(prediction_intervals_df.index))
-
         return prediction_intervals_df
 
 
-
-    def plot_forecast_prediction_intervals(self, pi_df: pd.DataFrame, id: str = "Sum") -> None:
+    def plot_forecast_prediction_intervals(self, pi_df: pd.DataFrame, ID: str = "Sum") -> None:
         """Plot forecasts for future time periods with prediction intervals calculated by MC Dropout using compute_model_uncertainty()
 
         Args:
             pi_df: A DataFrame of prediction intervals for forecasts, generated by compute_model_uncertainty()
-            id: The ID of the observation for which to have forecasts plotted, or "Sum" for aggregated counts.
+            ID: The ID of the observation for which to have forecasts plotted, or "Sum" for aggregated counts.
         """
 
-        ID = str(id)
+        ID = str(ID)
         forecast_df = pi_df[pi_df['ID'] == ID]
-
-
         if len(forecast_df) == 0:
             try:
                 ID = int(ID)
@@ -818,30 +794,20 @@ class TFModeler(Modeler):
                 ID = ID
                 warn("Invalid ID.", UserWarning)
                 return None
-
         forecast_df = forecast_df.assign(Period=forecast_df['Period'].tolist())
-
         forecast_df = forecast_df.rename({forecast_df.columns[2]: "Lower", forecast_df.columns[5]: "Upper"}, axis = "columns")
-
         plt.clf()
-
         plt.scatter('Period', 'Mean', data=forecast_df, color="black")
-
         plt.fill_between(x='Period', y1='Lower', y2='Upper',
                          data=forecast_df, color='gray', alpha=0.3,
                          linewidth=0)
-
         plt.xlabel("Period")
-
         if ID == "Sum":
             plt.ylabel("Expected counts")
         else:
             plt.ylabel("Survival probability")
-
         plt.title("Forecasts with Prediction Intervals")
-
         plt.show()
-
         return None
 
 
--- tests_performance/Data_Fabrication.py
+++ tests_performance/Data_Fabrication.py
@@ -6,38 +6,90 @@ import numpy as np
 import pandas as pd
 
 
-def make_person(i, N_PERIODS, k=0, exit_prob=0.5, exit_type_prob=None):
+
+def update_person(x1, x2, x3, exit_prob=None, exit_type_prob=None, dgp=1):
+    x2 = np.random.normal()
+    if dgp == 1:
+        # x3 += 0.1
+        if x1 == "A":
+            # exit_prob = exit_prob_base + 0.2
+            exit_type_prob = [0.7, 0.2, 0.1]
+        if x1 == "B":
+            # exit_prob += 0.1
+            exit_type_prob = [0.2, 0.7, 0.1]
+        if x1 == "C":
+            exit_type_prob = [0.1, 0.2, 0.7]
+            # x3 += 0.1
+    elif dgp == 2:
+        if x1 == "A":
+            exit_type_prob = [0.7, 0.2, 0.1]
+    elif dgp == 3:
+        if x1 == "A":
+            exit_type_prob = [0.95, 0.025, 0.025]  # [0.7, 0.2, 0.1]
+        if x1 == "B":
+            exit_type_prob = [0.025, 0.95, 0.025]  # [0.2, 0.7, 0.1]
+        if x1 == "C":
+            exit_type_prob = [0.025, 0.025, 0.95]  # [0.1, 0.2, 0.7]
+    else:
+        raise NameError('Invalid DGP')
+    return x1, x2, x3, exit_prob, exit_type_prob
+
+
+
+def make_person(i, N_PERIODS, k=0, exit_prob=0.5, exit_type_prob=None, dgp=1, beta_dict = None):
     df = []
     date = np.random.randint(N_PERIODS)
     x1 = np.random.choice(["A", "B", "C"])  # categorical variable
     x2 = np.random.normal()  # and a stationary continuous variable
-    x3 = np.random.uniform()  # trending variable
+    x3 = np.random.uniform()  # numerical variable fixed per i
     # initialize any other characteristics here
-    X = [np.random.normal() for i in range(k) if k > 0]
+    Z = [np.random.normal() for i in range(k) if k > 0]
     cols = ["ID", "period", "X1", "X2", "X3"] + [
         "".join("X" + str(i)) for i in range(4, k + 4) if k > 0
     ]
     exit_type = "No_exit"
-    exit_prob_base = exit_prob
-    while date <= N_PERIODS:
-        # print([i, date, x3, x1])
-        df.append([i, date, x1, x2, x3] + X)
-        if exit_type != "No_exit":
-            break
-        ### could make this part, below, its own function, or put the whole while loop in its own function
-        # this part handles characteristics that change over time
-        x3 += 0.1
-        x2 = np.random.normal()
+    if beta_dict is not None:
         if x1 == "A":
-            exit_prob = exit_prob_base + 0.2
-            exit_type_prob = [0.6, 0.3, 0.1]
-        if x1 == "B":
-            exit_prob += 0.1
+            beta_x1 = beta_dict['beta_xa']
+        elif x1 == "B":
+            beta_x1 = beta_dict['beta_xb']
         else:
-            x3 += 0.1
-        ###
-        exit_type = make_exit_type(exit_prob=exit_prob, p=exit_type_prob)
-        date += 1
+            beta_x1 = beta_dict['beta_xc']
+
+        # use Weibull distribution for survival curves
+        logh = beta_dict['beta_0'] + beta_dict['beta_t'] * np.log(beta_dict['t'] * (10 / N_PERIODS)) + beta_x1 + beta_dict['beta_x2'] * x2 + beta_dict['beta_x3'] * (x3 - 0.5)
+        p = 1 - np.exp(-(np.exp(logh)))
+        survival_prob = [1 - p[0]]
+        for tp in range(1,len(p)):
+            survival_prob.append(survival_prob[tp-1] * (1 - p[tp]))
+        time = 0
+        while date <= N_PERIODS:
+            exit_prob = p[min(time, N_PERIODS - 1)]
+            # print([i, date, x3, x1])
+            df.append([i, date, x1, x2, x3] + Z)
+            if exit_type != "No_exit":
+                break
+            # this part handles characteristics that change over time
+            x1, x2_temp, x3, exit_prob, exit_type_prob = update_person(x1, x2, x3, exit_prob=exit_prob,
+                                                                  exit_type_prob=exit_type_prob, dgp=dgp)
+            exit_type = make_exit_type(exit_prob=exit_prob, p=exit_type_prob)
+            date += 1
+            time += 1
+
+    else:
+        exit_prob_base = exit_prob
+        while date <= N_PERIODS:
+            # print([i, date, x3, x1])
+            df.append([i, date, x1, x2, x3] + Z)
+            if exit_type != "No_exit":
+                break
+            # this part handles characteristics that change over time
+            x1, x2, x3, exit_prob, exit_type_prob = update_person(x1, x2, x3, exit_prob=exit_prob,
+                                                                  exit_type_prob=exit_type_prob, dgp=dgp)
+            exit_type = make_exit_type(exit_prob=exit_prob, p=exit_type_prob)
+            date += 1
+
+
     df = pd.DataFrame(df, columns=cols)
     df["exit_type"] = exit_type
     return df
@@ -59,22 +111,51 @@ def make_exit_type(exit_prob=0.5, p=None):
 
 
 def fabricate_data(
-    N_PERSONS=100, N_PERIODS=10, SEED=None, k=0, exit_prob=0.5, exit_type_prob=None
+        N_PERSONS=100, N_PERIODS=10, SEED=None, k=0, exit_prob=0.5, exit_type_prob=None, dgp=1,
+        covariates_affect_outcome= False
 ):
     if SEED is not None:
         np.random.seed(SEED)
+    if covariates_affect_outcome:
+        t = np.arange(1,N_PERIODS + 1)
+        beta_t = np.random.uniform(low = 0, high = 1.2)
+        beta_0 = np.log((2 ** beta_t) * ((8) ** (-beta_t)) * np.log(1 / (1 - exit_prob)))
+        beta_xa = np.random.normal(scale = 0.5)
+        beta_xb = np.random.normal(scale = 0.5)
+        beta_xc = np.random.normal(scale = 0.5)
+        beta_x2 = np.random.normal(loc = -0.2, scale = 0.7)
+        beta_x3 = np.random.normal(scale = 0.7)
+        beta_dict = {
+            "beta_0": beta_0,
+            "beta_t": beta_t,
+            "beta_xa": beta_xa,
+            "beta_xb": beta_xb,
+            "beta_xc": beta_xc,
+            "beta_x2": beta_x2,
+            "beta_x3": beta_x3,
+            "t": t
+        }
+
+    else:
+        beta_dict = None
+
     df = make_person(
-        0, N_PERIODS, k=k, exit_prob=exit_prob, exit_type_prob=exit_type_prob
+        0, N_PERIODS, k=k, exit_prob=exit_prob, exit_type_prob=exit_type_prob, dgp=dgp,
+        beta_dict = beta_dict
     )
     for i in np.arange(1, N_PERSONS):
         temp = make_person(
-            i, N_PERIODS, k=k, exit_prob=exit_prob, exit_type_prob=exit_type_prob
+            i, N_PERIODS, k=k, exit_prob=exit_prob, exit_type_prob=exit_type_prob, dgp=dgp,
+            beta_dict = beta_dict
         )
         df = df.append(temp)
+    df = df.reset_index(drop=True)
     return df
 
 
 if __name__ == "__main__":
-    df = fabricate_data(N_PERSONS=10, N_PERIODS=4, SEED=1234)
-    print(df["exit_type"].value_counts())
-    df.to_csv("simulated_exit_data.csv", index=False)
+    df1 = fabricate_data(N_PERSONS=1000, N_PERIODS=4, SEED=1234, exit_prob=0.3, dgp=1)
+    df2 = fabricate_data(N_PERSONS=1000, N_PERIODS=4, SEED=1234, exit_prob=0.3, dgp=2)
+    print(df1.groupby(["X1", "exit_type"]).size())
+    print(df2.groupby(["X1", "exit_type"]).size())
+    # df.to_csv("simulated_exit_data.csv", index=False)
