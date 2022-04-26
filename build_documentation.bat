0e9386296423f6ed83a368f362954c108729eda9
James Bishop <james.mckown.bishop@gmail.com>
2022-04-26 08:09:07 -0500

    Fixed sphinx dependency version errors

--- build_documentation.bat
+++ build_documentation.bat
@@ -8,7 +8,10 @@ rem | You may need to specify your local path to activate.bat
 
 call C:/Users/%username%/Miniconda3/Scripts/activate.bat
 call C:/Users/%username%/AppData/Local/Continuum/anaconda3/Scripts/activate.bat
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --upgrade sphinx==2.4.4 sphinx_rtd_theme m2r
+call conda create -y -n fife_doc_env python=3.8
+call C:/Users/%username%/Miniconda3/Scripts/activate.bat fife_doc_env
+call C:/Users/%username%/AppData/Local/Continuum/anaconda3/Scripts/activate.bat fife_doc_env
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --upgrade sphinx==2.4.4 sphinx_rtd_theme m2r jinja2==3.0.3 mistune==0.8.4 
 cd docs/
 sphinx-build . _build/
 cmd /k
