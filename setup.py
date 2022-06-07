f812eeb35e520cb454abee247a7eb696724c130c
James Bishop <james.mckown.bishop@gmail.com>
2022-06-07 09:35:24 -0500

    Updated version to 1.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="fife",
-    version="1.6.0",
+    version="1.6.1",
     description=(
         "Finite-Interval Forecasting Engine: Machine learning models "
         "for discrete-time survival analysis and multivariate time series "
